30181f8680169193105587a2b05eac87699269bf
diesisfox <james.zhi.liu@gmail.com>
2017-08-24 08:31:59 -0400

    added temperatures

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function choosePort(){
 function displayFrame(frame){
 	frameBank[frame.id] = frame;
 	drawReport();
-	console.log(frame);
+	console.log('\n',frame);
 }
 
 function drawReport(){
@@ -53,43 +53,50 @@ function drawReport(){
 
 ${chalk.cyan('BATTERY POWER:')}
 Voltage: ${
-	frameBank[0x201] ? (frameBank[0x201].data.readInt32BE(0)/1E6).toFixed(3) : '?'
+	frameBank[0x201] ? chalk.yellow((frameBank[0x201].data.readInt32BE(0)/1E6).toFixed(3)) : '?'
 } V,	Current: ${
-	frameBank[0x201] ? (frameBank[0x201].data.readInt32BE(4)/1E6).toFixed(3) : '?'
+	frameBank[0x201] ? chalk.yellow((frameBank[0x201].data.readInt32BE(4)/1E6).toFixed(3)) : '?'
 } A,	Power: ${
-	frameBank[0x201] ? ((frameBank[0x201].data.readInt32BE(0)/1E6) * (frameBank[0x201].data.readInt32BE(4)/1E6)).toFixed(3) : '?'
+	frameBank[0x201] ? chalk.yellow(((frameBank[0x201].data.readInt32BE(0)/1E6) * (frameBank[0x201].data.readInt32BE(4)/1E6)).toFixed(3)) : '?'
 } W
 
 ${chalk.cyan('CELL VOLTAGES:')}
 ${
 	Array(9).join(0).split(0).map((item, i) => `L${Math.floor(i/3)}C${(i%3)*4+0}: ${
-		frameBank[0x350+i] ? (frameBank[0x350+i].data.readUInt16BE(0)/1E4).toFixed(3) : '?'
+		frameBank[0x350+i] ? chalk.yellow((frameBank[0x350+i].data.readUInt16BE(0)/1E4).toFixed(3)) : '?'
 	} V,	L${Math.floor(i/3)}C${(i%3)*4+1}: ${
-		frameBank[0x350+i] ? (frameBank[0x350+i].data.readUInt16BE(2)/1E4).toFixed(3) : '?'
+		frameBank[0x350+i] ? chalk.yellow((frameBank[0x350+i].data.readUInt16BE(2)/1E4).toFixed(3)) : '?'
 	} V,	L${Math.floor(i/3)}C${(i%3)*4+2}: ${
-		frameBank[0x350+i] ? (frameBank[0x350+i].data.readUInt16BE(4)/1E4).toFixed(3) : '?'
+		frameBank[0x350+i] ? chalk.yellow((frameBank[0x350+i].data.readUInt16BE(4)/1E4).toFixed(3)) : '?'
 	} V,	L${Math.floor(i/3)}C${(i%3)*4+3}: ${
-		frameBank[0x350+i] ? (frameBank[0x350+i].data.readUInt16BE(6)/1E4).toFixed(3) : '?'
-	} V
-`).join('')
+		frameBank[0x350+i] ? chalk.yellow((frameBank[0x350+i].data.readUInt16BE(6)/1E4).toFixed(3)) : '?'
+	} V`).join('\n')
 }
 
-${chalk.cyan('PPT POWER:')}
+${chalk.cyan('CELL TEMPERATURES:')}
+${
+	Array(16).join(0).split(0).map((item, i) => `${i*2+0}: ${
+		frameBank[0x500+i] ? chalk.yellow((frameBank[0x500+i].data.readInt32BE(0)/1E6).toFixed(3)) : '?'
+	} °C,	${i*2+1}: ${
+		frameBank[0x500+i] ? chalk.yellow((frameBank[0x500+i].data.readInt32BE(4)/1E6).toFixed(3)) : '?'
+	} °C,`).join('\t')
+}
+
+${chalk.cyan('PPT POWERS:')}
 ${
 	Array(3).join(0).split(0).map((item, i) => `PPT${i}: Voltage: ${
-		frameBank[0x20A+i] ? (frameBank[0x20A+i].data.readInt32BE(0)/1E6).toFixed(3) : '?'
+		frameBank[0x20A+i] ? chalk.yellow((frameBank[0x20A+i].data.readInt32BE(0)/1E6).toFixed(3)) : '?'
 	} V,	Current: ${
-		frameBank[0x20A+i] ? (frameBank[0x20A+i].data.readInt32BE(4)/1E6).toFixed(3) : '?'
+		frameBank[0x20A+i] ? chalk.yellow((frameBank[0x20A+i].data.readInt32BE(4)/1E6).toFixed(3)) : '?'
 	} A,	Power: ${
-		frameBank[0x20A+i] ? ((frameBank[0x20A+i].data.readInt32BE(0)/1E6) * (frameBank[0x201].data.readInt32BE(4)/1E6)).toFixed(3) : '?'
-	} W
-`).join('')
+		frameBank[0x20A+i] ? chalk.yellow(((frameBank[0x20A+i].data.readInt32BE(0)/1E6) * (frameBank[0x201].data.readInt32BE(4)/1E6)).toFixed(3)) : '?'
+	} W`).join('\n')
 }
 
 ${chalk.cyan('HEARTBEAT TIMESTAMPS:')}
 ${
 	Array(16).join(0).split(0).map((item, i) => `${i}: ${
-		frameBank[0x050+i] ? frameBank[0x050+i].timestamp.toLocaleTimeString() : '?'
+		frameBank[0x050+i] ? chalk.magenta(frameBank[0x050+i].timestamp.toLocaleTimeString()) : '?'
 	}, `).join('')
 }
 
--- test.js
+++ test.js
@@ -26,7 +26,7 @@ function ask(){
 function displayFrame(frame){
 	frameBank[frame.id] = frame;
 	drawReport();
-	console.log(frame);
+	console.log('\n',frame);
 }
 
 function drawReport(){
@@ -35,23 +35,23 @@ function drawReport(){
 
 ${chalk.cyan('BATTERY POWER:')}
 Voltage: ${
-	frameBank[0x201] ? (frameBank[0x201].data.readInt32BE(0)/1E6).toFixed(3) : '?'
+	frameBank[0x201] ? chalk.orange((frameBank[0x201].data.readInt32BE(0)/1E6).toFixed(3)) : '?'
 } V,	Current: ${
-	frameBank[0x201] ? (frameBank[0x201].data.readInt32BE(4)/1E6).toFixed(3) : '?'
+	frameBank[0x201] ? chalk.orange((frameBank[0x201].data.readInt32BE(4)/1E6).toFixed(3)) : '?'
 } A,	Power: ${
-	frameBank[0x201] ? ((frameBank[0x201].data.readInt32BE(0)/1E6) * (frameBank[0x201].data.readInt32BE(4)/1E6)).toFixed(3) : '?'
+	frameBank[0x201] ? chalk.orange(((frameBank[0x201].data.readInt32BE(0)/1E6) * (frameBank[0x201].data.readInt32BE(4)/1E6)).toFixed(3)) : '?'
 } W
 
 ${chalk.cyan('CELL VOLTAGES:')}
 ${
 	Array(9).join(0).split(0).map((item, i) => `L${Math.floor(i/3)}C${(i%3)*4+0}: ${
-		frameBank[0x350+i] ? (frameBank[0x350+i].data.readUInt16BE(0)/1E4).toFixed(3) : '?'
+		frameBank[0x350+i] ? chalk.orange((frameBank[0x350+i].data.readUInt16BE(0)/1E4).toFixed(3)) : '?'
 	} V,	L${Math.floor(i/3)}C${(i%3)*4+1}: ${
-		frameBank[0x350+i] ? (frameBank[0x350+i].data.readUInt16BE(2)/1E4).toFixed(3) : '?'
+		frameBank[0x350+i] ? chalk.orange((frameBank[0x350+i].data.readUInt16BE(2)/1E4).toFixed(3)) : '?'
 	} V,	L${Math.floor(i/3)}C${(i%3)*4+2}: ${
-		frameBank[0x350+i] ? (frameBank[0x350+i].data.readUInt16BE(4)/1E4).toFixed(3) : '?'
+		frameBank[0x350+i] ? chalk.orange((frameBank[0x350+i].data.readUInt16BE(4)/1E4).toFixed(3)) : '?'
 	} V,	L${Math.floor(i/3)}C${(i%3)*4+3}: ${
-		frameBank[0x350+i] ? (frameBank[0x350+i].data.readUInt16BE(6)/1E4).toFixed(3) : '?'
+		frameBank[0x350+i] ? chalk.orange((frameBank[0x350+i].data.readUInt16BE(6)/1E4).toFixed(3)) : '?'
 	} V
 `).join('')
 }
@@ -59,11 +59,11 @@ ${
 ${chalk.cyan('PPT POWER:')}
 ${
 	Array(3).join(0).split(0).map((item, i) => `PPT${i}: Voltage: ${
-		frameBank[0x20A+i] ? (frameBank[0x20A+i].data.readInt32BE(0)/1E6).toFixed(3) : '?'
+		frameBank[0x20A+i] ? chalk.orange((frameBank[0x20A+i].data.readInt32BE(0)/1E6).toFixed(3)) : '?'
 	} V,	Current: ${
-		frameBank[0x20A+i] ? (frameBank[0x20A+i].data.readInt32BE(4)/1E6).toFixed(3) : '?'
+		frameBank[0x20A+i] ? chalk.orange((frameBank[0x20A+i].data.readInt32BE(4)/1E6).toFixed(3)) : '?'
 	} A,	Power: ${
-		frameBank[0x20A+i] ? ((frameBank[0x20A+i].data.readInt32BE(0)/1E6) * (frameBank[0x201].data.readInt32BE(4)/1E6)).toFixed(3) : '?'
+		frameBank[0x20A+i] ? chalk.orange(((frameBank[0x20A+i].data.readInt32BE(0)/1E6) * (frameBank[0x201].data.readInt32BE(4)/1E6)).toFixed(3)) : '?'
 	} W
 `).join('')
 }
@@ -71,7 +71,7 @@ ${
 ${chalk.cyan('HEARTBEAT TIMESTAMPS:')}
 ${
 	Array(16).join(0).split(0).map((item, i) => `${i}: ${
-		frameBank[0x050+i] ? frameBank[0x050+i].timestamp.toLocaleTimeString() : '?'
+		chalk.magenta(frameBank[0x050+i] ? frameBank[0x050+i].timestamp.toLocaleTimeString()) : '?'
 	}, `).join('')
 }
 
